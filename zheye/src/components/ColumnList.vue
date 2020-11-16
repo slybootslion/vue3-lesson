f0edf1118393bb50a4511507d26df3b5c2d8ffad
slybootslion <slybootslion@vip.qq.com>
2020-11-16 19:05:38 +0800

    2020-11-16 19:05:38

--- zheye/src/components/ColumnList.vue
+++ zheye/src/components/ColumnList.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="column in list" :key="column.id">
+        <img :src="column.avatar" alt="" />
+        <h5>{{ column.title }}</h5>
+        <p>{{ column.description }}</p>
+        <a href="#"></a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
 
 export default defineComponent({
+  name: 'ColumnList',
   setup () {
     return {}
+  },
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
   }
 })
 </script>
